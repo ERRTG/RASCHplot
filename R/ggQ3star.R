b49aec01a45953af08959526311deb1642545fbf
Ann-Sophie Buchardt <asbu@sund.ku.dk>
2023-11-10 22:14:40 +0100

    Q3star vignette

--- R/ggQ3star.R
+++ R/ggQ3star.R
@@ -5,10 +5,9 @@
 #' @param object \code{Q3star} object, typically result of \link[RASCHplot]{Q3star}.
 #' @param method character, the visualization method of Q3star matrix to be
 #'   used. Allowed values are "circle" (default), "square".
-#' @param type character, "mixed" (default), "full", "lower" or "upper" display.
-#' @param markQ3star Logical flag for highlighting the \eqn{Q_{3\ast}}{Q3*} value by a rectangle.
-#' @param lower character, the visualisation method of lower triangular part of Q3star matrix to be used (only valid when \code{type = "mixed"}). Allowed values are "number" (default), "circle", "square".
-#' @param upper character, the visualisation method of upper triangular part of Q3star matrix to be used (only valid when \code{type = "mixed"}). Allowed values are "number", "circle" (default), "square".
+#' @param type character, "lower" (default), "upper", or "full" display.
+#' @param labels character, "upper" (default), "lower", "full" or "none"/NULL, Q3 value labels.
+#' @param markQ3star character, for highlighting the \eqn{Q_{3\ast}}{Q3*} value by a rectangle.
 #' @param ggtheme ggplot2 function or theme object. Default value is \code{theme_minimal}. Allowed values are the official ggplot2 themes including \code{theme_gray}, \code{theme_bw}, \code{theme_classic}, \code{theme_void}, .... Theme objects are also allowed (e.g., \code{theme_classic()}).
 #' @param title character, title of the graph.
 #' @param show.legend logical, if TRUE (default) the legend is displayed.
@@ -20,9 +19,8 @@
 #' @param colors a vector of 3 colors for low, mid and high correlation values.
 #' @param outline.color the outline color of square or circle. Default value is "gray".
 #' @param outline.width the outline width of square or circle. Default value is 0 (no outline).
-#' @param lab logical value. If TRUE, add correlation coefficient on the plot.
 #' @param lab_col,lab_size size and color to be used for the correlation
-#'   coefficient labels. used when lab = TRUE.
+#'   coefficient labels when showed.
 #' @param pch add character on the glyphs of Q3star value (only valid when markQ3star is TRUE). Default value is 4.
 #' @param pch.col,pch.cex the color and the cex (size) of pch (only valid when
 #'   markQ3star is TRUE).
@@ -31,7 +29,7 @@
 #' @param digits Decides the number of decimal digits to be displayed (Default is 2).
 #'
 #' @importFrom reshape2 melt
-#' @importFrom ggplot2 ggplot aes geom_tile geom_point scale_size guides scale_fill_gradient2 geom_text geom_label theme element_blank geom_label element_text coord_fixed theme_minimal
+#' @importFrom ggplot2 ggplot aes geom_tile geom_point scale_size guides scale_fill_gradient2 geom_text geom_label theme element_blank geom_label element_text coord_fixed theme_minimal scale_y_continuous scale_x_continuous
 #' @importFrom rlang .data
 #' @importFrom dplyr filter case_when
 #' @importFrom grDevices colorRampPalette
@@ -57,7 +55,7 @@
 #' # The \eqn{Q_{3\ast}}{Q3*} value is obtained for items D4 and D5:
 #' q3obj$Q3star
 #'
-#' # With the reasoning that these items holds the same information,
+#' # With the reasoning that these items hold the same information,
 #' # we create a new variable as the sum of D4 and D5:
 #' it.SPADI$D4D5 <- it.SPADI$D4 + it.SPADI$D5
 #' # and remove the two:
@@ -68,35 +66,42 @@
 #'
 #' @export
 #'
-ggQ3star <- function(object, method = c("circle", "square"), type = c("mixed", "full", "lower", "upper"), markQ3star = c("circle", "pch"), lower = NULL, upper = NULL, ggtheme = theme_minimal, title = "", show.legend = TRUE, legend.title = "Q3star", show.diag = NULL, colors = NULL, outline.color = NULL, outline.width = 0, lab = FALSE, lab_col = "black", lab_size = 4, pch = 8, pch.col = "black", pch.cex = 5, tl.cex = 12, tl.col = "black", tl.srt = 45, digits = 2) {
+ggQ3star <- function(object, method = c("circle", "square"), type = c("lower", "upper", "full"), labels = c(NA, "upper", "lower", "full", "none"), markQ3star = c("circle", "square", "pch", NA), ggtheme = theme_minimal, title = "", show.legend = TRUE, legend.title = "Q3star", show.diag = NULL, colors = NULL, outline.color = NULL, outline.width = 0, lab_col = "black", lab_size = 4, pch = 8, pch.col = "black", pch.cex = 2, tl.cex = 12, tl.col = "black", tl.srt = 45, digits = 2) {
 
   if (!inherits(object, "Q3star")) {
     stop("use only with \"Q3star\" objects")
   }
 
+
   markQ3star <- match.arg(markQ3star)
   type <- match.arg(type)
   method <- match.arg(method)
+  labels <- match.arg(labels)
+
+  if (is.na(labels)) {
+    labels <- ifelse(type == "lower", "upper",
+                     ifelse(type == "upper", "lower",
+                            ifelse(type == "full" & markQ3star != "pch", "full","none")))
+  }
+
+  if (type == "full") {
+    type <- c("lower", "upper")
+  }
+
+  if (labels == "full") {
+    labels <- c("lower", "upper")
+  }
 
   if (is.null(show.diag)) {
-    if (type == "full") {
+    if (all(type == c("lower", "upper"))) {
       show.diag <- TRUE
     } else {
       show.diag <- FALSE
     }
   }
 
-  if (type == "mixed") {
-    if (is.null(lower)) {
-      lower <- "number"
-    }
-    if (is.null(upper)) {
-      upper <- method
-    }
-  }
-
   if (is.null(colors)) {
-    colors <- c("#4575b4", "lightgray", "#B24745FF")
+    colors <- c("#4575B4", "lightgray", "#b24745")
   }
   if (is.null(outline.color)) {
     outline.color <- "gray"
@@ -118,7 +123,7 @@ ggQ3star <- function(object, method = c("circle", "square"), type = c("mixed", "
       .$rid > .$cid ~ "lower"
     )) %>%
     mutate(abs_corr = abs(.data$value),
-           value.lab = round(value, digits),
+           value.lab = round(.data$value, digits),
            lab = .data$Var1)
 
   #lims <- range(corr$value) * 1.1
@@ -127,9 +132,18 @@ ggQ3star <- function(object, method = c("circle", "square"), type = c("mixed", "
   maxabsval <- ceiling(max(corr$abs_corr)*100)/100
   lims <- c(-maxabsval, maxabsval)
 
-   p <- ggplot(data = corr %>% filter(part == "upper"),
-              mapping = aes(x = .data$cid,
-                            y = .data$rid,
+  tri.dat <- corr %>%
+    dplyr::filter(.$part %in% labels | .$value == object$Q3max - Q3mean)
+
+  if (markQ3star %in% c("circle", "square", "pch")) {
+    Q3stardat <- corr %>%
+      dplyr::filter(.$value == object$Q3max - Q3mean & !(.$part %in% c(type, "diag")))
+  }
+
+
+  p <- ggplot(data = corr %>% filter(part %in% type),
+              mapping = aes(x = .data$rid,
+                            y = .data$cid,
                             fill = .data$value))
 
    # add labels
@@ -140,13 +154,20 @@ ggQ3star <- function(object, method = c("circle", "square"), type = c("mixed", "
   if (method == "square") {
     p <- p +
       geom_tile(color = outline.color)
+    p <- p + geom_tile(data = Q3stardat,
+                       color = outline.color)
   } else if (method == "circle") {
 
     p <- p + geom_point(color = outline.color,
                         shape = 21, stroke = outline.width,
                         aes(size = .data$value))
+    p <- p + geom_point(data = Q3stardat,
+                        color = outline.color,
+                        shape = 21, stroke = outline.width,
+                        aes(size = .data$value))
   }
 
+   p <- p + guides(fill=guide_legend(title=legend.title))
   # adding colors
   p <- p + scale_fill_gradient2(
     low = colors[1],
@@ -154,60 +175,46 @@ ggQ3star <- function(object, method = c("circle", "square"), type = c("mixed", "
     mid = colors[2],
     midpoint = 0,
     limit = lims,
-    #name = legend.title,
     guide = "legend"
   )
 
+  p <- p + guides(size=guide_legend(title=legend.title))
+
   # Change guide
   p <- p +
-    #scale_fill_continuous(guide = "legend", limits = limits) +
-    #scale_size_continuous(range = limits) +
     scale_size(limits = lims, range = c(4, 14))
 
   # matrix cell glyphs
-  if (markQ3star == "pch") {
+  if (all(type == c("lower", "upper")) && markQ3star == "pch") {
     p <- p + geom_point(
-      mapping = aes(x = .data$Var1, y = .data$Var2),
+      data = corr %>%
+        dplyr::filter(.$value == object$Q3max - Q3mean),
+      mapping = aes(x = .data$rid, y = .data$cid),
       shape = pch,
       size = pch.cex,
-      color = pch.col
+      color = pch.col,
+      show.legend = FALSE
     )
   }
 
-  # adding values
-  if (type == "full") {
-
-    tri.dat <- corr %>%
-      dplyr::filter(part != "diag")
-
-  } else if (type == "lower" || lower == "number") {
-
-    tri.dat <- corr %>%
-      dplyr::filter(part == "lower")
-
-    if (markQ3star == "circle") {
-      Q3stardat <- corr %>%
-        dplyr::filter(.$value == object$Q3max - Q3mean & .$part == "upper")
-    }
-
-  } else if (type == "upper" || upper == "number") {
-
-    tri.dat <- corr %>%
-      dplyr::filter(part == "upper")
-
-    if (markQ3star == "circle") {
-      Q3stardat <- corr %>%
-        dplyr::filter(.$value == object$Q3max - Q3mean & .$part == "lower")
-    }
-
+  if (!is.null(labels) && !all(labels == "none")) {
+    p <- p +
+      geom_text(data = tri.dat,
+                aes(x = .data$rid, y = .data$cid), label = tri.dat$value.lab,
+                size = lab_size, color = lab_col) +
+      geom_text(data = Q3stardat,
+                aes(x = .data$rid, y = .data$cid), label = Q3stardat$value.lab,
+                size = lab_size, color = lab_col) +
+      theme(axis.text.x = element_blank(), axis.text.y = element_blank())
+  }
+  if (all(labels == c("lower", "upper"))) {
+    p <- p +
+      geom_text(data = tri.dat,
+                aes(x = .data$rid, y = .data$cid), label = tri.dat$value.lab,
+                size = lab_size, color = lab_col) +
+      theme(axis.text.x = element_blank(), axis.text.y = element_blank())
   }
 
-  p <- p +
-    geom_text(data = tri.dat,
-              aes(x = .data$cid, y = .data$rid), label = tri.dat$value.lab) +
-    geom_text(data = Q3stardat,
-              aes(x = .data$cid, y = .data$rid), label = Q3stardat$value.lab) +
-    theme(axis.text.x = element_blank(), axis.text.y = element_blank())
 
   # depending on the class of the object, add the specified theme
   if (class(ggtheme)[[1]] == "function") {
@@ -215,7 +222,6 @@ ggQ3star <- function(object, method = c("circle", "square"), type = c("mixed", "
   } else if (class(ggtheme)[[1]] == "theme") {
     p <- p + ggtheme
   }
-
 
   p <- p +
     theme(
